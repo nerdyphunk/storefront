2d9b2183847c576afa2716953317995bf6c18ff7
psychonoise <id206641178@gmail.com>
2025-09-01 19:16:46 +0300

    w7465735375g367865

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			out: "build",
+			precompress: false,
+			envPrefix: ["PUBLIC_"],
+		}),
 		alias: {
 			"@": "./src",
 		},
